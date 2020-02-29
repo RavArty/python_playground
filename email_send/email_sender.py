f3eac4f7ce76d46e9784459c0c564c7a6a89f9b5
Ravshan <ravkart@gmail.com>
2020-02-28 21:39:32 -0800

    email sender

---
+++ email_send/email_sender.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Rav Art'
+email['to'] = 'ravkart@gmail.com'
+email['subject'] = 'test email from py'
+
+email.set_content('I am here')
+
+with smtplib.SMTP(host='smtp.mail.yahoo.com', port=465) as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.login('email', 'pass')
+	smtp.send_message(email)
+	print('done')
