7b9586caaac283d8a4248d04a8a475faf1e04f8a
Ravshan <ravkart@gmail.com>
2020-04-04 12:32:50 -0700

    list of all articles with points > 99

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
+
+
+def sort_stories_by_vote(list):
+    return sorted(list, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
@@ -16,8 +21,9 @@ def create_custom_hn(links, subtext):
 
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-            hn.append({'title': title, 'link': href, 'votes': points})
-    return hn
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_vote(hn)
 
 
-print(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(links, subtext))
