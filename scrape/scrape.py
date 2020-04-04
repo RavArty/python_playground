8eebae57d4fcc2c3c0bdb84f8ec01c4c27f5cb61
Ravshan <ravkart@gmail.com>
2020-04-04 12:16:50 -0700

    printing dictionary with title, href and points

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -3,4 +3,21 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.title)
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+
+print(create_custom_hn(links, subtext))
