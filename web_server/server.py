4c98bcd5cc9b816de6f69b3df29c6636b4463c3c
Ravshan <ravkart@gmail.com>
2020-04-10 18:43:17 -0700

    fixed links for home and work

--- web_server/server.py
+++ web_server/server.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route('/works.html')
+def work():
+    return render_template('work.html')
