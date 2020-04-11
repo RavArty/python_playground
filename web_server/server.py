f639ba7d300423c59a2b7050502818b5eabf6005
Ravshan <ravkart@gmail.com>
2020-04-10 21:33:25 -0700

    dynamic pages

--- web_server/server.py
+++ web_server/server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/works.html')
-def work():
-    return render_template('work.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
